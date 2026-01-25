04316778541d1521f1040d29848173534cc1cfd3
Subhojit Das <das.subhojit@gmail.com>
2026-01-25 10:36:55 -0600

    Update

--- PythonBasics.py
+++ PythonBasics.py
@@ -1,3 +1,5 @@
 #This my 1st Python Programme
 '''Date is Sunday 25th Jan 2026'''
 print("Hello Subhojit!\nHow are you doing today?")
+place=input("Where are you now? ")
+print("Hello Subhojit! You are now in",place)
