15089a4b37616f93ed6b4a8769883aafd4b87805
Subhojit Das <das.subhojit@gmail.com>
2026-01-25 15:29:53 -0600

    initial

---
+++ Calculator.py
@@ -0,0 +1,6 @@
+'''This is basic calculator'''
+
+x=int(input("Please enter your 1st number\t").strip())
+y=int(input("Please enter your 2nd number\t").strip())
+
+print(f"Here is Sum of two number {x+y}")
